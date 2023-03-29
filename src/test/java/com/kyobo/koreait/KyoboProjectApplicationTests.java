f848e54490e62c1fabd163aceb3c1fbc45acbaec
SangYoung2 <tmskan8@naver.com>
2023-03-29 21:46:00 +0900

    230329 AWS 연동

--- src/test/java/com/kyobo/koreait/KyoboProjectApplicationTests.java
+++ src/test/java/com/kyobo/koreait/KyoboProjectApplicationTests.java
@@ -2,23 +2,13 @@ package com.kyobo.koreait;
 
 import com.kyobo.koreait.domain.vos.CartVO;
 import com.kyobo.koreait.mapper.UserMapper;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Log4j2
 @SpringBootTest
 class KyoboProjectApplicationTests {
-    @Autowired
-    UserMapper userMapper;
-
-    @Test
-    void contextLoads() {
-        CartVO vo = new CartVO();
-        vo.setUserEmail("test");
-        vo.setBookISBN("11");
-        vo.setBookCount(3);
-
-        userMapper.modify_cart_book_count(vo);
-    }
 
 }
