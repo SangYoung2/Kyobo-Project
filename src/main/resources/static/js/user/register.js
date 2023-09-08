7979ed71c046bc2ec48a802c94002a8aca79a89c
SangYoung2 <tmskan8@naver.com>
2023-09-08 16:06:18 +0900

    230908 회원가입 페이지 디자인 수정 및 정규식 추가

--- src/main/resources/static/js/user/register.js
+++ src/main/resources/static/js/user/register.js
@@ -70,35 +70,134 @@ function phoneNumber_authenticate_confirm(){
         .catch(reason => {console.log(reason)})
 }
 
-function email_authenticated(){
-    fetch(`/api/user/${UserEmail.value}`)
-        .then(value => value.text())
-        .then(value => {
-            if(value === 'true'){
-                alert('유저가 존재하기 때문에 불가능!')
-                EmailCheck = false;
-            }else {
-                alert('그 아이디 사용가능!')
-                EmailCheck = true;
-            }
-        })
-        .catch(reason => {console.log(reason)})
+// 회원정보 입력 시 올바르게 입력하는지 확인하는 정규식
+UserEmail.onchange = () => {
+    const idReg = RegExp(/^[a-zA-Z\d]{6,12}$/);
+    const comment = UserEmail.parentElement.parentElement.querySelector('span')
+
+    if (UserEmail.value.length === 0 || UserEmail.value === "" || RegExp(/\s/).test(UserEmail.value)) {
+        comment.innerText = "아이디를 입력해주세요."
+        comment.style.color = "red";
+    } else if (!idReg.test(UserEmail.value)) {
+        comment.innerText = "6~12자리의 영문자, 숫자로 입력해주세요."
+        comment.style.color = "red";
+    } else {
+        email_authenticated(UserEmail.value)
+    }
+
+    function email_authenticated(userEmail){
+        fetch(`/api/user/` + userEmail)
+            .then(value => value.text())
+            .then(value => {
+                if(value === 'true'){
+                    comment.innerText = "아이디가 이미 존재합니다."
+                    comment.style.color = "red";
+                    EmailCheck = false;
+                }else {
+                    comment.innerText = "사용가능한 아이디입니다."
+                    comment.style.color = "green"
+                    EmailCheck = true;
+                }
+            })
+            .catch(reason => {console.log(reason)})
+    }
 }
 
 Password.onchange = () => {
+    const pwReg = RegExp(/^[a-zA-Z\d!@#]{4,20}$/);
+    const comment = Password.nextElementSibling;
+
+    if(Password.value.length === 0 || Password.value === "" || RegExp(/\s/g).test(Password.value)) {
+        comment.innerText = "비밀번호를 입력해주세요."
+        comment.style.color = "red"
+    }
+    else if(!pwReg.test(Password.value)){
+        comment.innerText = "4~20자리의 영문자, 숫자, 특수문자(!,@,#)로 입력해주세요"
+        comment.style.color = "red"
+    }
+    else {
+        comment.innerText = "올바르게 입력하셨습니다."
+        comment.style.color = "green"
+    }
     passwordValue = Password.value;
 }
 
 PasswordConfirm.onchange = () => {
     passwordConfirmValue = PasswordConfirm.value;
-    console.log(passwordConfirmValue)
+    const comment = PasswordConfirm.nextElementSibling;
+
     if(passwordValue === passwordConfirmValue) {
-        console.log("true")
+        comment.innerText = "비밀번호가 일치합니다.";
+        comment.style.color = "green";
         PasswordCheck = true;
     }
     else {
-        console.log("false")
+        comment.innerText = "비밀번호가 일치하지 않습니다.";
+        comment.style.color = "red";
         PasswordCheck = false;
+    }
+}
+
+UserName.onchange = () => {
+    const nameReg = RegExp(/^[가-힣a-zA-Z]{2,15}$/);
+    const comment = UserName.nextElementSibling;
+
+    if(UserName.value.length === 0 || UserName.value === "" || RegExp(/\s/).test(UserName.value)){
+        comment.innerText = "이름을 입력해주세요."
+        comment.style.color = "red";
+    }
+    else if(!nameReg.test(UserName.value)){
+        comment.innerText = "2~15자리의 한글, 영문자로 입력해주세요."
+        comment.style.color = "red";
+    }
+    else {
+        comment.innerText = "올바르게 입력하셨습니다."
+        comment.style.color = "green"
+    }
+}
+
+Year.onchange = () => {
+    const yearReg = /\d{4}/
+    const comment = Year.parentElement.nextElementSibling;
+    console.log(typeof parseInt(Year.value))
+
+    if(Year.value.length === 0 || Year.value === "" || RegExp(/\s/g).test(Year.value)) {
+        comment.innerText = "년도를 입력해주세요.";
+        comment.style.color = "red";
+    }
+    else if(!yearReg.test(Year.value)) {
+        comment.innerText = "1900~2023년 사이로 숫자 4자리로 입력해주세요.";
+        comment.style.color = "red";
+    }
+    else if(!(Year.value <= 2023 && Year.value >= 1900)) {
+        comment.innerText = "1900~2023년 사이로 숫자 4자리로 입력해주세요.";
+        comment.style.color = "red";
+    }
+    else {
+        comment.innerText = "올바르게 입력하셨습니다.";
+        comment.style.color = "green";
+    }
+}
+
+Phone.onchange = () => {
+    const phoneReg = /\d{10,11}/g;
+    const comment = Phone.parentElement.nextElementSibling.nextElementSibling;
+
+    if(Phone.value.length === 0 || Phone.value === "" || RegExp(/\s/g).test(Phone.value)) {
+        comment.innerText = "핸드폰 번호를 입력해주세요"
+        comment.style.color = "red";
+    }
+    else if(!phoneReg.test(Phone.value)) {
+        comment.innerText = "'-'를 뺀 휴대번호 10~11자리를 입력해주세요"
+        comment.style.color = "red";
+    }
+    else if(!Phone.value.length >= 10) {
+        comment.innerText = "'-'를 뺀 휴대번호 10~11자리를 입력해주세요"
+        comment.style.color = "red";
+    }
+    else {
+        comment.innerText = "올바르게 입력하셨습니다."
+        comment.style.color = "green";
     }
 }
 
@@ -133,7 +232,7 @@ RegisterBtn.onclick = () => {
 
 // 돌아가기 버튼 눌렀을 때 (do back)
 BackBtn.onclick = () => {
-    const cancelConfirm = confirm('정말로 회원가입을 취소하고 돌아가시겠습니까?');
+    const cancelConfirm = confirm('회원가입창을 나가시겠습니까?');
     if(cancelConfirm) {
         location.href = '/user/login';
     }}
