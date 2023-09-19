64fd1302b3d4c65294e9792be57889d37014bfe5
SangYoung2 <tmskan8@naver.com>
2023-09-19 14:41:11 +0900

    230919 제품 상세 페이지 디자인 변경 및 기능 개선

--- src/main/resources/static/js/main/detail.js
+++ src/main/resources/static/js/main/detail.js
@@ -2,15 +2,18 @@ const ViewBtn = document.querySelector(".view_btn")
 const BookDetailContents = document.querySelector(".book_detail_contents")
 const BookDetailContent = document.querySelector(".book_detail_content")
 
+if(BookDetailContent.querySelectorAll("div").length < 10) {
+    ViewBtn.style.display = "none";
+}
+
+
 ViewBtn.onclick = () => {
 
     BookDetailContents.classList.toggle('on');
     if(BookDetailContents.classList.contains('on')) {
-        console.log("on")
         BookDetailContents.style.maxHeight  = window.getComputedStyle(BookDetailContent).height;
     }
     else {
-        console.log("off")
         BookDetailContents.style.maxHeight = "300px";
     }
 }
